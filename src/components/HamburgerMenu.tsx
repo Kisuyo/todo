230f1b8e88e9de04f1ec65d53ef695af63c75589
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-02-10 02:45:51 +0100

    .

--- src/components/HamburgerMenu.tsx
+++ src/components/HamburgerMenu.tsx
@@ -7,15 +7,15 @@ interface Props {
   setActive: (active: boolean) => void
 }
 
-export default function HamburgerMenu({ children, active, setActive }: Props) {
+export default function HamburgerMenu(props: Props) {
   return (
     <HamburgerMenuWrapper
       onClick={() => {
-        setActive(true)
+        props.setActive(true)
       }}
     >
-      {!active && <div>not active</div>}
-      {active && <div>active</div>}
+      {!props.active && <div>not active</div>}
+      {props.active && <div>active</div>}
       {/* <img src="menu.svg" /> */}
     </HamburgerMenuWrapper>
   )
