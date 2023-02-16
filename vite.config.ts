c99db98b085e97e9a14719451791fec50fddc08b
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-02-16 16:08:41 +0100

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite"
 import solidPlugin from "solid-start/vite"
 import solidStyled from "vite-plugin-solid-styled"
-import solid from "solid-start/vite"
-import vercel from "solid-start-vercel"
 
 export default defineConfig({
   plugins: [
@@ -13,9 +11,6 @@ export default defineConfig({
         include: "src/**/*.tsx",
         exclude: "node_modules/**/*.{ts,js}",
       },
-    }),
-    solid({
-      adapter: vercel({}),
     }),
   ],
 })
